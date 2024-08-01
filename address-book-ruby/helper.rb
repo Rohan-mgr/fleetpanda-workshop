11ffd4653015d47bea1bf05f81d183614ea0b706
Rohan-mgr <rohan.magar.415@gmail.com>
2024-08-01 14:38:08 +0545

    mapped user choice

--- address-book-ruby/helper.rb
+++ address-book-ruby/helper.rb
@@ -12,4 +12,37 @@ module Helper
     end
     return [@@FILE_PATH, @@data]
   end 
+
+  def map_main_menu_choice choice
+    choice_map = {
+      add_address_book: 1,
+      delete_address_book: 2,
+      display_all_address_books: 3,
+      update_address_book: 4,
+      search_contacts: 5,
+      exit: 6
+    } 
+    choice_map[choice.to_sym]
+  end
+
+  def map_search_menu_choice choice
+    choice_map = {
+      search_by_name: 1,
+      search_by_contact: 2,
+      search_by_address: 3,
+      exit_searching: 4,
+    } 
+    choice_map[choice.to_sym]
+  end
+
+  def map_update_menu_choice choice
+    choice_map = {
+      update_by_name: 1,
+      update_by_contact: 2,
+      update_by_address: 3,
+      update_all: 4,
+      exit_updating: 5,
+    } 
+    choice_map[choice.to_sym]
+  end
 end
